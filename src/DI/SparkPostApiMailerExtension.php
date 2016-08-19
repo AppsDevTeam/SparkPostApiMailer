a3b50ddede8178218a486af79fc0b8a29e195df1
Daniel Stříbrný <daniel@appsdevteam.com>
2016-08-19 11:48:55 +0200

    Disable autowiring for other IMailer services

--- src/DI/SparkPostApiMailerExtension.php
+++ src/DI/SparkPostApiMailerExtension.php
@@ -7,10 +7,17 @@ use ADT\SparkPostApiMailer\Services;
 
 class SparkPostApiMailerExtension extends \Nette\DI\CompilerExtension {
 
-	public function beforeCompile() {
-		parent::beforeCompile();
+	public function loadConfiguration() {
+		parent::loadConfiguration();
 
 		$builder = $this->getContainerBuilder();
+
+		// disable autowiring for all other IMailer services
+		foreach ($builder->getDefinitions() as $definition) {
+			if ($definition->getClass() === \Nette\Mail\IMailer::class) {
+				$definition->setAutowired(FALSE);
+			}
+		}
 
 		// register service
 		$builder->addDefinition($this->prefix('mailer'))
