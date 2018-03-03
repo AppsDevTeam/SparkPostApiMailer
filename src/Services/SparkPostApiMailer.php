234724ab7367bc669d80f33516acec09a16fa7db
Tomáš Kudělka <thorewi@gmail.com>
2018-03-03 12:27:24 +0100

    fix deprecated stuff

--- src/Services/SparkPostApiMailer.php
+++ src/Services/SparkPostApiMailer.php
@@ -2,6 +2,7 @@
 
 namespace ADT\SparkPostApiMailer\Services;
 
-class SparkPostApiMailer extends \Nette\Object implements \Nette\Mail\IMailer {
+class SparkPostApiMailer implements \Nette\Mail\IMailer {
+	use \Nette\SmartObject;
 	use \ADT\SparkPostApiMailer\SparkPostApiMailerTrait;
 }
